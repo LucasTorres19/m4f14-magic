5dad5aef487fca7e29a19caa52f1687680149932
Francisco Villella <franciscovillella2003@gmail.com>
2025-10-27 15:30:35 -0300

    add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
 import { HydrateClient } from "@/trpc/server";
-import CurrentMatch from "./current-match";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function Home() {
   return (
     <HydrateClient>
       <main className="flex min-h-screen">
-        <CurrentMatch />
+        <div className="flex h-screen w-screen flex-col items-center justify-center text-center">
+          <div className="space-y-10">
+            <h1 className="text-4xl font-bold">Bienvenidos</h1>
+            <h3>
+              Esta es la pagina del mafia para jugar magic wacho denle play y
+              baneen el mazo rojo que jugo lucas el otro dia amigo re jede nos
+              mato en 3seg
+            </h3>
+            <Link href={"/match"}>
+              <Button>Comenzar partida</Button>
+            </Link>
+          </div>
+        </div>
       </main>
     </HydrateClient>
   );
