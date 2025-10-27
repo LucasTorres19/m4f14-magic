3219b8a64932e49d3907e14bc19818ab5b17ed6d
Thiago <thiagovaldiviezogc@gmail.com>
2025-10-27 15:11:22 -0300

    add hp updates

--- src/app/_stores/use-current-match.ts
+++ src/app/_stores/use-current-match.ts
@@ -2,24 +2,55 @@ import { create } from "zustand";
 import { persist } from "zustand/middleware";
 import { BASE_SETTINGS } from "./use-settings";
 import { randomHexColor } from "@/utils/random";
-interface Player {
+export interface Player {
+  id: string;
+  displayName: string;
   hp: number;
   backgroundColor: string;
 }
 
 interface CurrentMatchState {
   players: Player[];
-  set: <T extends "players">(key: T, value: CurrentMatchState[T]) => void;
+
+  updatePlayer: (
+    playerId: string,
+    data:
+      | Partial<Omit<Player, "id">>
+      | ((player: Player) => Partial<Omit<Player, "id">>),
+  ) => void;
+
+  updateHp: (playerId: string, amount: number) => void;
+
+  setHp: (playerId: string, hp: number) => void;
 }
 
 export const useCurrentMatch = create<CurrentMatchState>()(
   persist(
-    (set) => ({
-      players: Array.from({ length: BASE_SETTINGS.playersCount }, () => ({
-        hp: BASE_SETTINGS.startingHp,
-        backgroundColor: randomHexColor(),
-      })),
-      set: (key, value) => set((state) => ({ ...state, [key]: value })),
+    (set, get) => ({
+      players: Array.from({ length: BASE_SETTINGS.playersCount }).map(
+        (_, i) => ({
+          id: crypto.randomUUID(),
+          displayName: `P${i}`,
+          hp: BASE_SETTINGS.startingHp,
+          backgroundColor: randomHexColor(),
+        }),
+      ),
+
+      updatePlayer: (playerId, data) =>
+        set((state) => ({
+          players: state.players.map((player) =>
+            player.id === playerId
+              ? typeof data === "function"
+                ? { ...player, ...data(player) }
+                : { ...player, ...data }
+              : player,
+          ),
+        })),
+
+      updateHp: (playerId, amount) =>
+        get().updatePlayer(playerId, (player) => ({ hp: player.hp + amount })),
+
+      setHp: (playerId, hp) => get().updatePlayer(playerId, { hp }),
     }),
     { name: "current-match-store" },
   ),
--- src/app/current-match.tsx
+++ src/app/current-match.tsx
@@ -1,34 +1,44 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { useCurrentMatch } from "./_stores/use-current-match";
+import { useCurrentMatch, type Player } from "./_stores/use-current-match";
 import { Minus, Plus } from "lucide-react";
+
+function PlayerCurrentMatch({ player }: { player: Player }) {
+  const updateHp = useCurrentMatch((state) => state.updateHp);
+  return (
+    <div
+      style={{ backgroundColor: player.backgroundColor }}
+      className="text-background relative flex items-stretch justify-center overflow-hidden rounded-3xl text-9xl"
+    >
+      <Button
+        size="icon-lg"
+        className="h-full grow rounded-none pr-12"
+        variant="ghost"
+        onClick={() => updateHp(player.id, -1)}
+      >
+        <Minus className="text-background/60 size-8" strokeWidth={4} />
+      </Button>
+      <div className="pointer-events-none absolute flex h-full w-full items-center justify-center">
+        <button className="pointer-events-auto">{player.hp}</button>
+      </div>
+      <Button
+        size="icon-lg"
+        className="h-full grow rounded-none pl-12"
+        variant="ghost"
+        onClick={() => updateHp(player.id, 1)}
+      >
+        <Plus className="text-background/60 size-8" strokeWidth={4} />
+      </Button>
+    </div>
+  );
+}
 export default function CurrentMatch() {
   const players = useCurrentMatch((state) => state.players);
 
   return (
     <div className="bg-background grid grow grid-cols-2 gap-3">
-      {players.map((player, index) => (
-        <div
-          style={{ backgroundColor: player.backgroundColor }}
-          className="text-background flex items-stretch justify-center overflow-hidden rounded-3xl text-9xl"
-          key={index}
-        >
-          <Button
-            size="icon-lg"
-            className="h-full grow rounded-none"
-            variant="ghost"
-          >
-            <Minus className="text-background/60 size-8" strokeWidth={4} />
-          </Button>
-          <button>{player.hp}</button>
-          <Button
-            size="icon-lg"
-            className="h-full grow rounded-none"
-            variant="ghost"
-          >
-            <Plus className="text-background/60 size-8" strokeWidth={4} />
-          </Button>
-        </div>
+      {players.map((player) => (
+        <PlayerCurrentMatch player={player} key={player.id} />
       ))}
     </div>
   );
