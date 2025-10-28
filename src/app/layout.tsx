a9cc3de814d99cba04271a87909ea64147efb16c
Thiago <thiagovaldiviezogc@gmail.com>
2025-10-28 19:11:01 -0300

    add uploadthing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
 import "@/styles/globals.css";
 
+import { ourFileRouter } from "@/app/api/uploadthing/core";
 import { Toaster } from "@/components/ui/sonner";
 import { TRPCReactProvider } from "@/trpc/react";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
 import { type Metadata } from "next";
 import { Geist } from "next/font/google";
+import { extractRouterConfig } from "uploadthing/server";
 import { CurrentMatchPovider } from "./_stores/current-match-provider";
 import { SettingsPovider } from "./_stores/settings-provider";
 
@@ -24,6 +27,15 @@ export default function RootLayout({
   return (
     <html lang="es" className={`${geist.variable} dark`}>
       <body className={`font-sans antialiased`}>
+        <NextSSRPlugin
+          /**
+           * The `extractRouterConfig` will extract **only** the route configs
+           * from the router to prevent additional information from being
+           * leaked to the client. The data passed to the client is the same
+           * as if you were to fetch `/api/uploadthing` directly.
+           */
+          routerConfig={extractRouterConfig(ourFileRouter)}
+        />
         <TRPCReactProvider>
           <SettingsPovider>
             <CurrentMatchPovider>{children}</CurrentMatchPovider>
--- src/env.js
+++ src/env.js
@@ -12,6 +12,7 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+    UPLOADTHING_TOKEN: z.string(),
   },
 
   /**
@@ -31,6 +32,7 @@ export const env = createEnv({
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     TURSO_AUTH_TOKEN: process.env.TURSO_AUTH_TOKEN,
+    UPLOADTHING_TOKEN: process.env.UPLOADTHING_TOKEN,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
